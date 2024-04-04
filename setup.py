11eba08cb456e7b48c828b287d1762c7e41a1381
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-04-04 10:56:24 +0100

    Add package data to make assay data collection easier

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='phytochempy',
     version='0.1',
-    packages=find_packages(),
-    package_data={},
+    packages=find_packages(include=['phytochempy'], exclude=['library_info_and_data_import']),
+    package_data={"phytochempy": ["compound_properties/inputs/*"]},
     install_requires=[
         'pandas>=2.1.4',
-        'numpy>=1.26'
+        'numpy',
+        'urllib',
+        'cirpy'
     ],
-    extras_require={
-    },
+    extras_require={'knapsack': ["html5lib", 'beautifulsoup4','tqdm'],
+                    'metabolite_metrics': ['rdkit', 'chembl_webresource_client'],
+                    },
     url='https://github.com/alrichardbollans/PhytoChemicalDiversity',
     license='GNU v.3',
     author='Adam Richard-Bollans',
