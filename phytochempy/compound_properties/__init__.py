307e7919c9007df856bbc206e92de222428064e4
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-04-05 10:03:58 +0100

    Move package

--- phytochempy/compound_properties/__init__.py
+++
@@ -1,5 +0,0 @@
-from .generic_compound_functions import *
-from .antiplasmodial import *
-from .classyfire_methods import *
-from .drug_likeness import *
-from .npclassifier_methods import *
--- phytochempy/data_compilation_utilities/__init__.py
+++
@@ -1,1 +0,0 @@
-from .compile_data_methods import *
