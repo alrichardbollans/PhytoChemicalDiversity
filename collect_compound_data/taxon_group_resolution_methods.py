e307c56660e0a82e8a8ffb5ba0e5a54794e91411
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-04-08 14:18:26 +0100

    Add genus llevel versions

---
+++ collect_compound_data/taxon_group_resolution_methods.py
@@ -0,0 +1,190 @@
+import math
+import os
+from typing import List, Tuple, Any
+
+import pandas as pd
+from pandas import DataFrame, Series
+from phytochempy.compound_properties import get_npclassifier_result_columns_in_df, sanitize_filename
+from wcvp_download import wcvp_accepted_columns
+
+from collect_compound_data import all_taxa_compound_csv, FAMILIES_OF_INTEREST, NP_PATHWAYS, COMPOUND_ID_COL
+
+
+def get_processed_metabolite_data(taxa_compound_data: pd.DataFrame, comp_id_col: str, taxon_grouping: str, families: List[str]) -> pd.DataFrame:
+    """
+    :param taxa_compound_data: A pandas DataFrame containing the taxa and compound data.
+    :param comp_id_col: The name of the column in taxa_compound_data containing the compound IDs.
+    :param taxon_grouping: The name of the column in taxa_compound_data containing the taxon grouping information.
+    :param families: A list of strings representing the families to filter the data by.
+    :return: A processed pandas DataFrame containing the filtered metabolite data.
+
+    """
+    # Remove records without necessary data, as well as duplicates
+    taxa_compound_data = taxa_compound_data.dropna(subset=[comp_id_col, taxon_grouping], how='any')
+
+    cleaned = taxa_compound_data.drop_duplicates(
+        subset=[taxon_grouping, comp_id_col],
+        keep='first')
+
+    processed_metabolite_data = cleaned[cleaned[wcvp_accepted_columns['family']].isin(families)]
+    processed_metabolite_data['NPclassif_pathway_results'] = processed_metabolite_data['NPclassif_pathway_results'].apply(sanitize_filename)
+
+    pathway_cols = get_npclassifier_pathway_columns_in_df(processed_metabolite_data)
+    pathways = []
+    for p in pathway_cols:
+        processed_metabolite_data[p] = processed_metabolite_data[p].apply(sanitize_filename)
+        pathways += processed_metabolite_data[p].dropna().tolist()
+    pathways = set(pathways)
+    assert len(pathways) == 7
+    print(pathways)
+    return processed_metabolite_data
+
+
+def get_npclassifier_pathway_columns_in_df(df: pd.DataFrame) -> List[str]:
+    """
+    :param df: A pandas DataFrame containing NPclassifier result columns.
+    :return: A list of pathway columns in the given DataFrame.
+    """
+    pathway_cols = get_npclassifier_result_columns_in_df(df)
+    cols = []
+    for p in pathway_cols:
+        if 'pathway' in p and p != 'NPclassif_pathway_results':
+            cols.append(p)
+    return cols
+
+
+def separate_into_pathway(df: pd.DataFrame, pathway: str) -> tuple[DataFrame, Any]:
+    """
+    Separate the given DataFrame into two separate DataFrames based on a specified pathway.
+
+    :param df: The input DataFrame.
+    :param pathway: The pathway to separate the DataFrame by.
+    :return: A tuple containing two separate DataFrames: positives and negatives.
+    """
+    df = df.dropna(subset=['NPclassif_pathway_results'])
+    pathway_cols = get_npclassifier_pathway_columns_in_df(df)
+
+    positives = pd.DataFrame()
+    for col in pathway_cols:
+        positives = pd.concat([positives, df[df[col] == pathway]])
+    negatives = df[~df.index.isin(positives.index)]
+    assert len(positives) + len(negatives) == len(df)
+    problems = positives[~positives['NPclassif_pathway_results'].str.contains(pathway)]
+    assert len(problems) == 0
+    problems = negatives[negatives['NPclassif_pathway_results'].str.contains(pathway)]
+    assert len(problems) == 0
+
+    return positives, negatives
+
+
+def add_pathway_information_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Add pathway information columns to a DataFrame.
+
+    :param df: The DataFrame to add pathway information columns to.
+    :return: The DataFrame with pathway information columns added.
+    """
+    original_length = len(df)
+
+    for pathway in NP_PATHWAYS:
+        relevant_paths, other_paths = separate_into_pathway(df, pathway)
+        relevant_paths[pathway] = 1
+        other_paths[pathway] = 0
+
+        pathway_df = pd.concat([relevant_paths, other_paths])[[COMPOUND_ID_COL, pathway]]
+        pathway_df = pathway_df.drop_duplicates(subset=[COMPOUND_ID_COL, pathway])
+        amibiguous_duplicates = pathway_df[pathway_df[COMPOUND_ID_COL].duplicated(keep=False)]
+        if len(amibiguous_duplicates) > 0:  # A check that comps with same ID have been assigned same class
+            print(f'WARNING: Some ambiguity for pathway: {pathway}. This is likely due to differing smiles strings for same given {COMPOUND_ID_COL}.')
+            print(amibiguous_duplicates)
+
+            raise ValueError
+        df = df.merge(pathway_df, how='left', on=COMPOUND_ID_COL)
+        assert len(df) == original_length  # Check no additions from merge
+
+    return df
+
+
+def get_genus_level_version_for_pathway(df: pd.DataFrame, comp_class: str, taxon_grouping='Genus'):
+    """
+    :param df: A pandas DataFrame containing the data
+    :param comp_class: A string specifying the column name of the class
+    :param taxon_grouping: A string specifying the taxon grouping column name (default is 'Genus')
+    :return: A pandas DataFrame containing genus level version for pathway
+
+    This method calculates the genus level version for a given pathway based on the provided DataFrame and parameters.
+    It performs several transformations and computations on the DataFrame to derive the final result.
+    """
+    expected_mean = df[comp_class].mean()
+
+    genera_df = df.copy()
+
+    num_genera_tested = len(genera_df[taxon_grouping].unique().tolist())
+
+    # count labelled species
+    counts = genera_df.value_counts(taxon_grouping)
+    counts.name = 'identified_compounds_count'
+    genera_df = pd.merge(genera_df, counts, how='left', left_on=taxon_grouping, right_index=True)
+    N_class_col = f'identified_{comp_class}_count'
+    genera_df[N_class_col] = genera_df.groupby([taxon_grouping])[comp_class].transform('sum')
+    mean_col = f'mean_identified_as_{comp_class}'
+    genera_df[mean_col] = genera_df.groupby([taxon_grouping])[comp_class].transform('mean')
+    expected_mean_col = f'expected_total_mean_for_{comp_class}'
+    genera_df[expected_mean_col] = expected_mean
+
+    # Normalised mean for some analysis following:
+    # Daniele Micci-Barreca, ‘A Preprocessing Scheme for High-Cardinality Categorical Attributes in Classification and Prediction Problems’,
+    # ACM SIGKDD Explorations Newsletter 3, no. 1 (July 2001): 27–32, https://doi.org/10.1145/507533.507538.
+    # The means for each class are highly unreliable for small counts
+    # We can use a blend of posterior and prior probabilties to improve this.
+    # In below, k (as in original paper) determines half of the sample size for which we trust the mean estimate
+    # f denotes the smoothing effect to balance categorical average vs prior. Higher value means stronger regularization.
+    # Here we use the defaults used in the target encoder library
+    def weighting_factor(given_val: float, k: int = 20, f: float = 10) -> float:
+        denom = 1 + (math.e ** ((k - given_val) / f))
+        return 1 / denom
+
+    factor_col = f'weigthing_factor_for_{comp_class}'
+    genera_df[factor_col] = genera_df['identified_compounds_count'].apply(weighting_factor)
+
+    norm_mean_col = f'norm_mean_identified_as_{comp_class}'
+    genera_df[norm_mean_col] = (genera_df[factor_col] * genera_df[mean_col]) + (
+            1 - genera_df[factor_col]) * expected_mean
+
+    genera_df = genera_df[
+        [taxon_grouping, 'identified_compounds_count', N_class_col, mean_col, expected_mean_col, factor_col,
+         norm_mean_col]]
+    genera_df = genera_df.reset_index(drop=True)
+
+    genera_df = genera_df.drop_duplicates(subset=[taxon_grouping])
+    genera_df.reset_index(drop=True, inplace=True)
+    assert len(genera_df) == num_genera_tested
+    return genera_df
+
+
+def get_genus_level_version_for_all_pathways(df: pd.DataFrame, taxon_grouping='Genus') -> pd.DataFrame:
+    ## Generate genus data for all pathways
+    out_df = pd.DataFrame()
+    out_df[taxon_grouping] = df[taxon_grouping].unique()
+    original_length = len(out_df)
+    for pathway in NP_PATHWAYS:
+        genus_pathway_df = get_genus_level_version_for_pathway(df, pathway, taxon_grouping=taxon_grouping)
+        genus_pathway_df = genus_pathway_df[
+            ['Genus', 'identified_compounds_count', f'identified_{pathway}_count', f'mean_identified_as_{pathway}']]
+        if 'identified_compounds_count' not in out_df.columns:
+            out_df = pd.merge(out_df, genus_pathway_df, on=['Genus'], how='left')
+        else:
+            out_df = pd.merge(out_df, genus_pathway_df, on=['Genus', 'identified_compounds_count'], how='left')
+    assert len(out_df) == original_length
+
+    return out_df
+
+
+if __name__ == '__main__':
+    my_df = pd.read_csv(all_taxa_compound_csv, index_col=0)
+    processed = get_processed_metabolite_data(my_df, 'SMILES', 'Genus', FAMILIES_OF_INTEREST)
+    processed_with_pathway_columns = add_pathway_information_columns(processed)
+    processed_with_pathway_columns.to_csv(os.path.join('outputs', 'processed_with_pathway_columns.csv'))
+    processed_with_pathway_columns.describe(include='all').to_csv(os.path.join('outputs', 'processed_pathway_summary.csv'))
+    genus_pathway_data = get_genus_level_version_for_all_pathways(processed_with_pathway_columns)
+    genus_pathway_data.to_csv(os.path.join('outputs', 'genus_level_pathway_data.csv'))
