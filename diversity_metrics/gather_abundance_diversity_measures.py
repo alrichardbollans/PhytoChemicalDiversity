59e10a1a80a4a069f98ca237725f91570f070f8e
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-04-15 17:42:25 +0100

    Rename indices

--- diversity_metrics/gather_abundance_diversity_measures.py
+++ diversity_metrics/gather_abundance_diversity_measures.py
@@ -14,13 +14,13 @@ def get_pathway_based_diversity_measures_for_genera(measure_df: pd.DataFrame, pa
     ## Read data for all pathways into
 
     ### Begin with Shannon index
-    measure_df['shannon'] = 0
+    measure_df['H'] = 0
     for pathway in pathways:
         measure_df[f'ln_mean_identified_as_{pathway}'] = np.log(measure_df[f'mean_identified_as_{pathway}']).replace(-np.inf, 0)
-        measure_df['shannon'] = measure_df['shannon'] + measure_df[f'mean_identified_as_{pathway}'] * measure_df[
+        measure_df['H'] = measure_df['H'] + measure_df[f'mean_identified_as_{pathway}'] * measure_df[
             f'ln_mean_identified_as_{pathway}']
 
-    measure_df['shannon'] = -measure_df['shannon']
+    measure_df['H'] = -measure_df['H']
     ## Bias corrected shannon
     # From chao_nonparametric_2003, following beck_comparing_2010.
     # Note that there are updated metrics for calculating coverage e.g. chao_coveragebased_2012
@@ -29,7 +29,7 @@ def get_pathway_based_diversity_measures_for_genera(measure_df: pd.DataFrame, pa
         measure_df.loc[measure_df[f'identified_{pathway}_count'] == 1, 'number_singletons'] += 1
     measure_df['sample_coverage'] = 1 - (measure_df['number_singletons'] / measure_df['identified_compounds_count'])
 
-    measure_df['bc_shannon'] = 0
+    measure_df['Hbc'] = 0
     for pathway in pathways:
         ###New log
         measure_df[f'ln_Cmean_identified_as_{pathway}'] = np.log(
@@ -37,16 +37,16 @@ def get_pathway_based_diversity_measures_for_genera(measure_df: pd.DataFrame, pa
         addition = ((measure_df[f'mean_identified_as_{pathway}'] * measure_df['sample_coverage'] * measure_df[f'ln_Cmean_identified_as_{pathway}']) /
                     (1 - (1 - measure_df[f'mean_identified_as_{pathway}'] * measure_df['sample_coverage']) ** measure_df[
                         'identified_compounds_count'])).fillna(0)
-        measure_df['bc_shannon'] = measure_df['bc_shannon'] + addition
-    measure_df['bc_shannon'] = -measure_df['bc_shannon']
+        measure_df['Hbc'] = measure_df['Hbc'] + addition
+    measure_df['Hbc'] = -measure_df['Hbc']
 
     # Simpson index also refered to as gini-simpson
     # Used in e.g. corre_evaluation_2023
-    measure_df['simpson'] = 0
+    measure_df['G'] = 0
     for pathway in pathways:
-        measure_df['simpson'] = measure_df['simpson'] + (
+        measure_df['G'] = measure_df['G'] + (
                 measure_df[f'mean_identified_as_{pathway}'] * measure_df[f'mean_identified_as_{pathway}'])
-    measure_df['simpson'] = 1 - measure_df['simpson']
+    measure_df['G'] = 1 - measure_df['G']
 
     measure_df['number_of_apparent_categories'] = 0
     for pathway in pathways:
@@ -57,14 +57,14 @@ def get_pathway_based_diversity_measures_for_genera(measure_df: pd.DataFrame, pa
 
     ## Pielou index normalises by the 'richness' for the given genus, i.e. the number of different pathways present
     ## as discussed in corre_evaluation_2023.
-    measure_df['pielou'] = measure_df['shannon'] / (np.log(measure_df['number_of_apparent_categories']))
-    measure_df['pielou'] = measure_df['pielou'].fillna(measure_df['shannon'])
+    measure_df['J'] = measure_df['H'] / (np.log(measure_df['number_of_apparent_categories']))
+    measure_df['J'] = measure_df['J'].fillna(measure_df['H'])
 
 
 
-    measure_df = measure_df[['Genus', 'shannon', 'bc_shannon', 'simpson', 'pielou']]
+    measure_df = measure_df[['Genus', 'H', 'Hbc', 'G', 'J']]
     from sklearn.preprocessing import MinMaxScaler
-    for index in ['shannon', 'bc_shannon', 'simpson', 'pielou']:
+    for index in ['H', 'Hbc', 'G', 'J']:
         scaler = MinMaxScaler()
         measure_df[index+'_minmax'] =scaler.fit_transform(measure_df[[index]])
         print(index)
