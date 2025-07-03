6939137cbab27b895f0eebf7b1d82a24a1496f4d
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2025-07-03 16:17:12 +0100

    Provide better analysis of underprioritised_regions

--- analysis_of_diversity_in_native_regions/helper_functions.py
+++ analysis_of_diversity_in_native_regions/helper_functions.py
@@ -24,3 +24,27 @@ def get_working_data():
             raise ValueError
     working_data = working_data.rename(columns={'phylogenetic_diversity': 'PD', 'number_of_species_in_group': 'SR'})
     return working_data
+
+
+def _multiple_loess(metric):
+    ## not using this
+    # From Cappellari et al., ‘The ATLAS3D Project – XX. Mass–Size and Mass–σ Distributions of Early-Type Galaxies’.
+    # 2D version is based on Cleveland and Devlin, ‘Locally Weighted Regression: An Approach to Regression Analysis by Local Fitting’.
+    reg_data = get_reg_data(metric)
+    x, y, z = reg_data['PD'].values, reg_data['SR'].values, reg_data[metric].values
+    zout, wout = loess_2d(x, y, z, xnew=None, ynew=None, degree=1, frac=0.5,
+                          npoints=None, rescale=False, sigz=None)
+    r2 = r2_score(z, zout)
+
+    analysis_df = reg_data
+    analysis_df['expected_diversity'] = zout
+    analysis_df['wout'] = wout
+    analysis_df['r2'] = r2
+    analysis_df[f'residuals'] = analysis_df[metric] - analysis_df['expected_diversity']
+
+    std_residual = analysis_df[f'residuals'].std()
+    mean_residual = analysis_df[f'residuals'].mean()
+    analysis_df['highlight_high'] = analysis_df[f'residuals'] > ((2 * std_residual) + mean_residual)
+    analysis_df['highlight_low'] = analysis_df[f'residuals'] < (mean_residual - (2 * std_residual))
+
+    return zout, wout, r2, analysis_df
--- analysis_of_diversity_in_native_regions/underprioritised_regions.py
+++ analysis_of_diversity_in_native_regions/underprioritised_regions.py
@@ -4,12 +4,11 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from loess.loess_2d import loess_2d
-from matplotlib import pyplot as plt
 import statsmodels.api as sm
-from sklearn.metrics import r2_score
-from sklearn.preprocessing import PolynomialFeatures
+from matplotlib import pyplot as plt
+
 from analysis_of_diversity_in_native_regions.helper_functions import get_working_data
+from collect_and_compile_data.collect_compound_data import COMPOUND_ID_COL
 from collect_and_compile_data.get_diversity_metrics.gather_diversity_measures import METRICS, RARE_METRICS
 
 outdir = os.path.join('outputs', 'underprioritised_regions')
@@ -23,46 +22,23 @@ def get_reg_data(metric):
     return reg_data
 
 
-def multiple_loess(metric):
-    # From Cappellari et al., ‘The ATLAS3D Project – XX. Mass–Size and Mass–σ Distributions of Early-Type Galaxies’.
-    # 2D version is based on Cleveland and Devlin, ‘Locally Weighted Regression: An Approach to Regression Analysis by Local Fitting’.
-    reg_data = get_reg_data(metric)
-    x, y, z = reg_data['PD'].values, reg_data['SR'].values, reg_data[metric].values
-    zout, wout = loess_2d(x, y, z, xnew=None, ynew=None, degree=1, frac=0.5,
-                          npoints=None, rescale=False, sigz=None)
-    r2 = r2_score(z, zout)
-
-    analysis_df = reg_data
-    analysis_df['expected_diversity'] = zout
-    analysis_df['wout'] = wout
-    analysis_df['r2'] = r2
-    analysis_df[f'residuals'] = analysis_df[metric] - analysis_df['expected_diversity']
-
-    std_residual = analysis_df[f'residuals'].std()
-    mean_residual = analysis_df[f'residuals'].mean()
-    analysis_df['highlight_high'] = analysis_df[f'residuals'] > ((2 * std_residual) + mean_residual)
-    analysis_df['highlight_low'] = analysis_df[f'residuals'] < (mean_residual - (2 * std_residual))
-
-    return zout, wout, r2, analysis_df
-
-
 def plot_2d_annotated_regression_data(data, metric_outpath, x_var, y_var):
     # Set up the plot
     import seaborn as sns
-    from pypalettes import load_cmap
+    # from pypalettes import load_cmap
     # plt.figure(figsize=(10, 6))
     # sns.set_style("whitegrid")
     # Scatter plot for APWD vs phylogenetic_diversity
     # colors = load_cmap('inferno').hex
     # print(colors)
-    data['color'] = np.where((data['highlight_high'] == True), '#d12020', 'grey')
-    data['color'] = np.where((data['highlight_low'] == True), '#5920ff', data['color'])
+    data['color'] = np.where((data[f'{y_var}_highlight_high'] == True), '#d12020', 'grey')
+    data['color'] = np.where((data[f'{y_var}_highlight_low'] == True), '#5920ff', data['color'])
 
     sns.scatterplot(x=x_var, y=y_var, data=data, color=data['color'], edgecolor="black", alpha=0.8)
 
     # Highlight points where 'highlight' is True
 
-    highlighted_data = data[(data['highlight_high'] == True) | (data['highlight_low'] == True)]
+    highlighted_data = data[(data[f'{y_var}_highlight_high'] == True) | (data[f'{y_var}_highlight_low'] == True)]
 
     for _, row in highlighted_data.iterrows():
         upshift = 0
@@ -92,17 +68,18 @@ def plot_2d_annotated_regression_data(data, metric_outpath, x_var, y_var):
         #     if row['Group'] in ['NZN']:
         #         upshift = 0.04
         #         left_shift = -0.50
-
-        plt.annotate(row['Group'], (row[x_var] + left_shift, row[y_var] + upshift), ha='right', color='black')
+        if row['Group'] == 'KZN':
+            left_shift = -0.05
+            plt.annotate(row['Group'], (row[x_var] + left_shift, row[y_var] + upshift), ha='right', color='black')
 
     # Line plot for expected_diversity vs xvar
 
     ## Add estimator to smooth cases where multiple observations of the y variable at the same x
-    sns.lineplot(x=x_var, y='expected_diversity', estimator='mean', color='black', linestyle='--', data=data)
+    sns.lineplot(x=x_var, y=f'{y_var}_loess_prediction', estimator='mean', color='black', linestyle='--', data=data)
 
     # Labels and legend
 
-    plt.xlabel(x_var)
+    plt.xlabel('Phylogenetic Diversity')
 
     plt.ylabel(y_var)
 
@@ -116,19 +93,16 @@ def plot_2d_annotated_regression_data(data, metric_outpath, x_var, y_var):
     sns.reset_orig()
 
 
-def find_regression_model(metric):
+def get_loess_outputs(metric):
     regression_out_dir = os.path.join(outdir, 'regressions', metric)
     os.makedirs(regression_out_dir, exist_ok=True)
     reg_data = get_reg_data(metric)
     y_var = metric
     x_var = 'PD'
-    # Step 1: Fit a linear regression model
-    X = reg_data[[x_var]].values  # Independent variable
-    X_to_plot = reg_data[x_var].values  # Independent variable (SR)
+    # X = reg_data[[x_var]].values  # Independent variable
+    X_to_plot = reg_data[x_var].values  # Independent variable
     # scaled_data[metric] = np.log(scaled_data[metric])
-    y = reg_data[y_var].values  # Dependent variable (diversity)
-
-    data = []
+    y = reg_data[y_var].values  # Dependent variable
 
     # Fit LOESS with outlier robustness (iterations downweight outliers)
     loess_prediction = sm.nonparametric.lowess(exog=X_to_plot, endog=y, return_sorted=False)
@@ -140,17 +114,60 @@ def find_regression_model(metric):
     r_squared = 1 - (ss_res / ss_tot)
     print(f"R² (Coefficient of Determination): {r_squared:.3f}")
 
-    sns.scatterplot(x=X_to_plot, y=y, edgecolor="black", alpha=0.8)
-    sns.lineplot(x=X_to_plot, y=expected_diversity, color='black', linestyle='--')
-    plt.xlabel('Phylogenetic Diversity')
-    plt.ylabel(metric)
-    plt.savefig(os.path.join(regression_out_dir, 'LOESS.jpg'), dpi=300)
-    plt.close()
-    data.append([f'LOESS', r_squared])
+    # sns.scatterplot(x=X_to_plot, y=y, edgecolor="black", alpha=0.8)
+    # sns.lineplot(x=X_to_plot, y=expected_diversity, color='black', linestyle='--')
+    # plt.xlabel('Phylogenetic Diversity')
+    # plt.ylabel(metric)
+    # plt.savefig(os.path.join(regression_out_dir, 'LOESS.jpg'), dpi=300)
+    # plt.close()
 
-    df = pd.DataFrame(data, columns=['Model', 'R-squared'])
-    df.to_csv(os.path.join(regression_out_dir, 'model_comparison.csv'))
-    return loess_prediction
+    out_df = reg_data
+    out_df[f'{metric}_loess_prediction'] = loess_prediction
+    out_df[f'{metric}_residuals'] = out_df[metric] - out_df[f'{metric}_loess_prediction']
+
+    std_residual = out_df[f'{metric}_residuals'].std()
+    mean_residual = out_df[f'{metric}_residuals'].mean()
+    out_df[f'{metric}_highlight_high'] = out_df[f'{metric}_residuals'] > ((2 * std_residual) + mean_residual)
+    out_df[f'{metric}_highlight_low'] = out_df[f'{metric}_residuals'] < (mean_residual - (2 * std_residual))
+    out_df['R2'] = r_squared
+    out_df.to_csv(os.path.join(regression_out_dir, 'loess_outputs.csv'))
+    plot_2d_annotated_regression_data(out_df, regression_out_dir, 'PD', metric)
+
+    return out_df
+
+
+def get_info_about_a_region(region):
+    out_data = []
+    all_region_data = pd.read_csv(
+        os.path.join('..', 'collect_and_compile_data', 'get_diversity_metrics', 'outputs', 'group_info',
+                     'native_regions.csv'))
+    region_data = all_region_data[all_region_data['Assigned_group'] == region]
+
+    species_in_region = region_data['accepted_species'].unique().tolist()
+    all_compound_data = pd.read_csv(
+        os.path.join('..', 'collect_and_compile_data', 'collect_compound_data', 'outputs',
+                     'all_species_compound_data.csv'))[['accepted_species', COMPOUND_ID_COL]]
+    all_compound_data = all_compound_data.drop_duplicates(subset=['accepted_species', COMPOUND_ID_COL])
+
+    region_compound_data = all_compound_data[all_compound_data['accepted_species'].isin(species_in_region)]
+    number_compounds_from_region = len(region_compound_data[COMPOUND_ID_COL].unique().tolist())
+
+    out_data.append([number_compounds_from_region])
+    extra_cols = []
+    for sp in species_in_region:
+        species_compounds = region_compound_data[region_compound_data['accepted_species'] == sp]
+        number_compounds_from_sp = len(species_compounds[COMPOUND_ID_COL].unique().tolist())
+        out_data.append([number_compounds_from_sp])
+        extra_cols.append([f'number_compounds_from_{sp}'])
+        species_regions = all_region_data[all_region_data['accepted_species'] == sp]
+        number_regions_for_species = len(species_regions['Assigned_group'].unique().tolist())
+        out_data.append([number_regions_for_species])
+        extra_cols.append(f'number_regions_for_{sp}')
+
+    out_df = pd.DataFrame(out_data, index=['number_compounds_from_region'] + extra_cols)
+    outpath = os.path.join(outdir, 'region_info')
+    Path(outpath).mkdir(parents=True, exist_ok=True)
+    out_df.to_csv(os.path.join(outpath, f'{region}_info.csv'))
 
 
 def main():
@@ -159,22 +176,39 @@ def main():
         metric_outpath = os.path.join(outdir, 'regressions', m)
         Path(metric_outpath).mkdir(parents=True, exist_ok=True)
 
-        zout, wout, r2, analysis_df = multiple_loess(m)
-        analysis_df.to_csv(os.path.join(metric_outpath, f'loess_outputs.csv'))
-        plot_2d_annotated_regression_data(analysis_df, metric_outpath, 'PD', m)
-        plot_2d_annotated_regression_data(analysis_df, metric_outpath, 'SR', m)
+        metric_analysis_df = get_loess_outputs(m)
 
-        highlights = analysis_df[(analysis_df['highlight_high'] == True)]['Group'].tolist()
+        highlights = metric_analysis_df[(metric_analysis_df[f'{m}_highlight_high'] == True)]['Group'].tolist()
         collected_highlights[m] = highlights
+    print(collected_highlights)
     consistent_compound_highlights = set(collected_highlights['APWD'])
-    for m in ['APWD']:
+    for m in ['FAD', 'MFAD', 'APWD']:
         consistent_compound_highlights = consistent_compound_highlights.intersection(collected_highlights[m])
-
     print(consistent_compound_highlights)
+
+    consistent_rare_compound_highlights = set(collected_highlights['APWD_Rare'])
+    for m in ['FAD_Rare', 'MFAD_Rare', 'APWD_Rare']:
+        consistent_rare_compound_highlights = consistent_rare_compound_highlights.intersection(collected_highlights[m])
+    print(consistent_rare_compound_highlights)
+
     consistent_pathway_highlights = set(collected_highlights['H'])
     for m in ['H', 'Hbc', 'G']:
         consistent_pathway_highlights = consistent_pathway_highlights.intersection(collected_highlights[m])
     print(consistent_pathway_highlights)
+
+    consistent_rare_pathway_highlights = set(collected_highlights['G_Rare'])
+    for m in ['H_Rare', 'Hbc_Rare', 'G_Rare']:
+        consistent_rare_pathway_highlights = consistent_rare_pathway_highlights.intersection(collected_highlights[m])
+    print(consistent_rare_pathway_highlights)
+
+    for r in consistent_pathway_highlights.union(consistent_compound_highlights).union(
+            consistent_rare_compound_highlights).union(consistent_rare_pathway_highlights):
+        get_info_about_a_region(r)
+
+    out_df = pd.DataFrame([str(consistent_compound_highlights), str(consistent_rare_compound_highlights),
+                           str(consistent_pathway_highlights), str(consistent_rare_pathway_highlights)],
+                          index=['compounds', 'rare_compounds', 'pathways', 'rare_pathways'])
+    out_df.to_csv(os.path.join(outdir, 'highlights.csv'))
 
 
 if __name__ == '__main__':
