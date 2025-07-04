bb023891aca21b8b82c739f1d08d27b455485593
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2025-07-04 16:20:33 +0100

    Add full histogram

--- collect_and_compile_data/collect_compound_data/do_good_data_summaries.py
+++ collect_and_compile_data/collect_compound_data/do_good_data_summaries.py
@@ -14,6 +14,11 @@ def histogram_of_count_per_species():
     data_df = compound_data[['accepted_species', COMPOUND_ID_COL]].drop_duplicates()
     group_size = data_df.groupby(['accepted_species']).size()
     pd.DataFrame(group_size).to_csv(os.path.join(out_dir, 'counts_per_species.csv'))
+    sns.histplot(x=group_size, kde=True)
+    plt.xlabel('Number of Compounds')
+    plt.savefig(os.path.join(out_dir, 'histogram_of_count_per_species.png'))
+    plt.close()
+
     group_size = group_size[group_size <= 100]
     sns.histplot(x=group_size, kde=True)
     plt.xlabel('Number of Compounds')
